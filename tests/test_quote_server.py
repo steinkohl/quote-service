9ca59147a63ca775f160302b2ea347f859564cd6
anastasiah00 <94566042+anastasiah00@users.noreply.github.com>
2022-02-19 18:00:21 +0100

    implemented test for invalid url

--- tests/test_quote_server.py
+++ tests/test_quote_server.py
@@ -9,11 +9,16 @@ def mock_get_quotes_from_url(values):
 
 
 class Test(TestCase):
+
     @mock.patch("quote_server._get_quotes_from_url", mock_get_quotes_from_url)
     def test_get_random_quote(self):
         actual = get_random_quote()
         expected = '"This is a mocked quote." - Unit Tester'
+        self.assertEqual(expected, actual)
 
+    def test_get_random_quote_invalid_url(self):
+        actual = get_random_quote("invalid_url")
+        expected = '"An error does not become a mistake until you refuse to correct it." - John F. Kennedy'
         self.assertEqual(expected, actual)
 
 
